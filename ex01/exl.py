5b2a48807c5cdec081b3cd07bc8a66c73abc960d
pierrelzw <pierrezhiwei@gmail.com>
2018-05-25 22:52:15 +0800

    add first exercice ex01

---
+++ ex01/exl.py
@@ -0,0 +1,15 @@
+#-*- coding: utf-8 -*-
+
+print("Hello World!"
+        )
+print("Hello Again")
+
+print("I like typing this")
+
+print("Yay! Printing.")
+
+print("I'd much rather you 'not'")
+
+print('I "said" do no touch this.')
+
+

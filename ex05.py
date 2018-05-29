27106193c3371c2ceaeda153e464d58554b5583f
pierrelzw <pierrezhiwei@gmail.com>
2018-05-29 23:12:12 +0800

    add ex05.py and ex06.py

---
+++ ex05.py
@@ -0,0 +1,21 @@
+# ex05: More variables and printing
+
+my_name = 'Zhwei LI'
+my_age = 23
+my_height = 175
+my_weight = 70
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilogram heavy.")
+print("Yay, that's not too heavy. I'm healthy!")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffe.")
+
+# This line is trickym try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+

9c1aa57e0395cf2e61c21a2d72e773386d5785c1
pierrelzw <pierrezhiwei@gmail.com>
2018-05-29 08:40:33 +0800

    add ex03_drills.py

--- ex03_drills.py
+++ ex03_drills.py
@@ -1,36 +1,29 @@
 # /Users/pierre_pc/anaconda3/bin/python
 # -*- coding: utf-8 -*-
 
-# ex03: Numbers and Math
+# ex03_drills: Numbers and Math in float
 
 print("I will now count my chickens:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
-
-print("Now I will count the eggs:")
-
 # calulate (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
 
 print("Is it true that 3 + 2 < 5 - 7?")
 
 # calulate(3 - 2 < 5 - 7)
-print(3 - 2 < 5 - 7)
+print(3.0 - 2.0 < 5.0 - 7.0)
 
-print("what is 3 + 2?", 3 + 2)
+print("what is 3.0 + 2.0?", 3.0 + 2.0)
 
-print("what is 5 - 7?", 5 - 7)
+print("what is 5.0 - 7.0?", 5.0 - 7.0)
 
 print("Oh, that's why it's false.")
 
 print("How about some more?")
 
-print("Is it greater? 5 > -2?", 5 > -2)
+print("Is it greater? 5.0 > -2.0?", 5.0 > -2.0)
 
-print("Is it greater or equal? 5 >= -2?", 5 >= -2)
+print("Is it greater or equal? 5.0 >= -2.0?", 5.0 >= -2.0)
 
-print("Is it less or equal? 5 <= -2?", 5 <= -2)
-
-
+print("Is it less or equal? 5.0 <= -2.0?", 5.0 <= -2.0)
 
